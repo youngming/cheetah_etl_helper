11ea84e29adca22e509c78605380d3e92cde4f44
Sam.Shi <sam.shi@aldi.cn>
2020-11-18 20:38:09 +0800

    fix bug in file walker

--- etl/helper/utils/common/file_operation.py
+++ etl/helper/utils/common/file_operation.py
@@ -8,6 +8,13 @@ def read_txt(file_path):
         txt = ''.join(ls_item)
         return txt
 
+def test(home_folder):
+    for current_folder, child_folder, file_name_list in os.walk(home_folder): 
+        print(current_folder)
+        print(child_folder)
+        #print(file_name_list)
+        print('=======================')
+
 def search_files_in_folder(home_folder, target_folder = None, target_file_type = None):
     for current_folder, child_folder, file_name_list in os.walk(home_folder): 
         if ((target_folder != None and (current_folder.split('/')[-1] == target_folder)) or (target_folder == None)):
@@ -17,9 +24,9 @@ def search_files_in_folder(home_folder, target_folder = None, target_file_type =
             else :
                 yield [current_folder + '/' + file_name for file_name in filter (lambda file_name : file_name.split('.')[-1] == target_file_type, file_name_list)]
         #fetch next layer folder
-        if (len(child_folder) > 0):
-            for child in child_folder:
-                yield from search_files_in_folder(child, target_folder, target_file_type)
+        # if (len(child_folder) > 0):
+        #     for child in child_folder:
+        #         yield from search_files_in_folder(child, target_folder, target_file_type)
 
 def delete_folders(home_folder, target_folder):
     for current_folder, child_folder, file_name_list in os.walk(home_folder): 
@@ -27,17 +34,19 @@ def delete_folders(home_folder, target_folder):
             shutil.rmtree(current_folder)
             return
         #fetch next layer folder
-        elif (len(child_folder) > 0):
-            for child in child_folder:
-                delete_folders(current_folder + '/' + child, target_folder)
-        else:
-            return
+        # elif (len(child_folder) > 0):
+        #     for child in child_folder:
+        #         delete_folders(current_folder + '/' + child, target_folder)
+        # else:
+        #     return
 
 def delete_file(path):
     if os.path.exists(path):
         os.remove(path)
 
 if __name__ == '__main__':
-    #files = [x for x in search_files_in_folder('/home/sam/works/cheetah_etl/src', target_folder = 'ops')]
-    delete_folders('/home/sam/works/cheetah_etl', 'yml')
-    #print(files)
+    files = [x for x in search_files_in_folder('/home/sam/works/cheetah_etl/src')]
+    # delete_folders('/home/sam/works/cheetah_etl', 'yml')
+    print(files)
+
+    # test('/home/sam/works/cheetah_etl/src')
